b18bde6fcc2e595d11afc3851d637f5bccbce0be
csteinberg <csteinberg@geekhive.com>
2018-12-07 16:57:08 -0500

    Working on footer

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export const Footer = () => (
+  <div className="">
+    <p>Copyright &copy; 2018 <strong>Cara Steinberg</strong></p>
+  </div>
+)
